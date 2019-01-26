562b374d20eb0b81f03033b9b9efdc929cc4fd13
Anthony Seo <anthonyhseo@gmail.com>
2019-01-25 16:54:31 -0800

    refactored and debugged code

--- BusSchedule.java
+++ BusSchedule.java
@@ -21,11 +21,10 @@ public class BusSchedule {
         in = new BufferedReader(new InputStreamReader(ct.getInputStream()));
     }
 
-    public static void main(String[] args) throws Exception {
-        init();
-
+    private static String getDestination() throws Exception {
         String inputLine = "";
         String text = "";
+
         while ((inputLine = in.readLine()) != null) {
             text += inputLine;
         }
@@ -40,33 +39,33 @@ public class BusSchedule {
 
         while (matcher.find()) {
             String dest = matcher.group(1);
-            dest = dest.replaceAll("&amp;", "&");
+            
             System.out.println("Destination: " + dest);
 
             String routes = matcher.group(2);
             Pattern routePattern = Pattern.compile("<a href=.*?>(.*?)</a>");
             Matcher routeMatcher = routePattern.matcher(routes);
-            // System.out.println(matcher.group(2));
             while (routeMatcher.find()) {
                 System.out.println("Bus Number: " + routeMatcher.group(1));
             }
             System.out.println("+++++++++++++++++++++++++++++++++++");
-
         }
 
         System.out.print("Please enter a route ID as a string: ");
         String routeNumber = sc.nextLine();
-        routeNumber = routeNumber.replaceAll("/", "-");
+        return routeNumber;
+    }
 
+    public static void printRouteStops(String routeNumber) throws Exception {
         String routeURL = "https://www.communitytransit.org/busservice/schedules/route/";
         routeURL += routeNumber;
         System.out.println("\nThe link for your route is: " + routeURL + "\n");
 
         busRouteURL = new URL(routeURL).openConnection();
-        // busRouteURL.setRequestProperty("user-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
         busRouteURL.setRequestProperty("user-Agent", requestProp);
         busRouteIn = new BufferedReader(new InputStreamReader(busRouteURL.getInputStream()));
 
+        String inputLine = "";
         String routeHtml = "";
         while ((inputLine = busRouteIn.readLine()) != null) {
             routeHtml += inputLine;
@@ -78,22 +77,33 @@ public class BusSchedule {
         Matcher routeMatcher = routePattern.matcher(routeHtml);
 
         while (routeMatcher.find()) {
-            System.out.println("Destination: " + routeMatcher.group(1));
-            // System.out.println(routeMatcher.group(2));
+            String dest = routeMatcher.group(1);
+            dest = dest.replaceAll("&amp;", "&");
+            System.out.println("Destination: " + dest);
             String stops = routeMatcher.group(2);
+            
 
             String stopRegex = ".*?<strong class=\"fa fa-stack-1x\">(.*?)</strong>.*?";
             stopRegex += "<p>(.*?)</p>.*?";
             Pattern stopPattern = Pattern.compile(stopRegex);
             Matcher stopMatcher = stopPattern.matcher(stops);
             while (stopMatcher.find()) {
+                String stop = stopMatcher.group(2);
+                stop = stop.replaceAll("&amp;", "&");
                 System.out.print("Stop Number: ");
-                System.out.println(stopMatcher.group(1) + " is " + stopMatcher.group(2));
+                System.out.println(stopMatcher.group(1) + " is " + stop);
             }
 
             System.out.println("+++++++++++++++++++++++++++++++++++");
         }
+    }
 
+    public static void main(String[] args) throws Exception {
+        init();
+
+        String routeNumber = getDestination();
+        routeNumber = routeNumber.replaceAll("/", "-");
+        printRouteStops(routeNumber);
 
         sc.close();
     }
